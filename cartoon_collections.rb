1449d37eb276c8b766216192f4d8889f54a0fea9
Marc Mills <mdmills1990@gmail.com>
2018-08-22 16:05:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,9 +20,8 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.find do |snack|
+  array.find do |snack|
     snack == cheese_types
-  else
-    "nil"
   end
+return "nil"
 end

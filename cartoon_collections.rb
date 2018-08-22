f85c50c5ebac87aeb1d20cd8588fe73150ad8992
Marc Mills <mdmills1990@gmail.com>
2018-08-22 16:04:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,12 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.find do |snack|
+    snack == cheese_types
+  else 
+    "nil"
+  end
+end
 end

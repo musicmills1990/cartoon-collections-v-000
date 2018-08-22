7de242f3ee2841c6627edfb5ad323aae0662e521
Marc Mills <mdmills1990@gmail.com>
2018-08-22 16:44:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_the_cheese(array)
     cheese_types = ["cheddar", "gouda", "camembert"]
-    array.each do |snack|
+    array.detect do |snack|
     snack.include?(cheese_types)
     end
 end
